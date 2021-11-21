953712d0276464859224050c88ff909d4c936d7d
Yarden92 <yarden9292@gmail.com>
2021-11-21 19:08:14 +0200

    Update auxiliary.py

--- FNFTpy/auxiliary.py
+++ FNFTpy/auxiliary.py
@@ -47,7 +47,8 @@ def get_lib_path():
 
     """
     #libstr = "/usr/local/lib/libfnft.so.0.2.2-dev"
-    libstr = "/usr/local/lib/libfnft.so.0.4.1"
+#     libstr = "/usr/local/lib/libfnft.so.0.4.1"
+    libstr = "FNFT/lib/libfnft.so.0.4.1"
 
     return libstr
 
